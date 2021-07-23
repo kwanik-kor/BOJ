904e58b7491d57a7e0af436475ada5996ad63022
kwanik-kor <rhksdlr134@naver.com>
2021-07-23 15:50:39 +0900

    Problem Solved: 1580번 위치 바꾸기

--- src/bfs/bfs_01580_changePosition.java
+++ src/bfs/bfs_01580_changePosition.java
@@ -5,6 +5,9 @@ import java.util.*;
 
 /**
  * 1. 문제 링크 : https://www.acmicpc.net/problem/1580
+ * 2. 풀이
+ *  - 클래스를 생성해서 Queue에 집어넣었을 때, 계속해서 메모리 초과가 났다.
+ *   > 이로 인해 visit처리를 위한 고정배열을 선언해서 처리하니 통과~
  */
 public class bfs_01580_changePosition {
 
@@ -13,27 +16,23 @@ public class bfs_01580_changePosition {
     static int[] dx = {0, 0, 1, 0, -1, 1, -1, 1, -1};
 
     static char[][] map;
-    static Point A, B;
-    static HashMap<Integer, Set<Integer>> visit, visitB;
+    static int ay, ax, by, bx;
 
     /*
         조건 1 : 한 턴에 하나 또는 두명이 움직일 수 있음
           > 움직임은 만들 수 있으나, 방문처리를 어떻게 할 것인가
      */
     static int solve(Movement m) {
+        boolean[][][][] visit = new boolean[N][M][N][M];
         Queue<Movement> q = new LinkedList<>();
         q.add(m);
-        visit.get(m.a.y * M + m.a.x).add(m.b.y * M + m.b.x);
-        visitB.get(m.b.y * M + m.b.x).add(m.a.y * M + m.a.x);
+        visit[m.ay][m.ax][m.by][m.bx] = true;
 
         while(!q.isEmpty()) {
             Movement now = q.poll();
-            if(now.a.y == B.y && now.a.x == B.x && now.b.y == A.y && now.b.x == A.x) {
+            if(now.ay == by && now.ax == bx && now.by == ay && now.bx == ax) {
                 return now.n;
             }
-
-            Point a = now.a;
-            Point b = now.b;
 
             for(int i = 0; i < 9; i++) {
                 for(int j = 0; j < 9; j++) {
@@ -41,14 +40,11 @@ public class bfs_01580_changePosition {
                     // 둘 다 멈춰 있는 경우는 Pass
                     if(i == 0 && j == 0) continue;
 
-                    int nyA = a.y + dy[i];
-                    int nxA = a.x + dx[i];
+                    int nyA = now.ay + dy[i];
+                    int nxA = now.ax + dx[i];
 
-                    int nyB = b.y + dy[j];
-                    int nxB = b.x + dx[j];
-
-                    Point nextA = new Point(nyA, nxA);
-                    Point nextB = new Point(nyB, nxB);
+                    int nyB = now.by + dy[j];
+                    int nxB = now.bx + dx[j];
 
                     /*
                         1. 범위를 벗어나거나 벽인 경우
@@ -58,14 +54,13 @@ public class bfs_01580_changePosition {
                     if(isOutOfRange(nyA, nxA)
                             || isOutOfRange(nyB, nxB)
                             || (nyA == nyB && nxA == nxB)
-                            || (nyA == b.y && nxA == b.x && nyB == a.y && nxB == a.x))
+                            || (nyA == now.by && nxA == now.bx && nyB == now.ay && nxB == now.ax))
                         continue;
 
-                    if(visit.get(nyA * M  + nxA).contains(nyB * M + nxB) || visitB.get(nyB * M + nxB).contains(nyA * M + nxA)) continue;
+                    if(visit[nyA][nxA][nyB][nxB]) continue;
 
-                    visit.get(nyA * M + nxA).add(nyB * M + nxB);
-                    visitB.get(nyB * M + nxB).add(nyA * M + nxA);
-                    q.add(new Movement(nextA, nextB, now.n + 1));
+                    visit[nyA][nxA][nyB][nxB] = true;
+                    q.add(new Movement(nyA, nxA, nyB, nxB, now.n + 1));
                 }
             }
         }
@@ -85,26 +80,22 @@ public class bfs_01580_changePosition {
         M = Integer.parseInt(st.nextToken());
 
         map = new char[N][M];
-        visit = new HashMap<>();
-        visitB = new HashMap<>();
         for(int i = 0; i < N; i++) {
             map[i] = br.readLine().toCharArray();
             for(int j = 0; j < M; j++) {
                 if(map[i][j] == 'A') {
-                    A = new Point(i, j);
+                    ay = i;
+                    ax = j;
                     map[i][j] = '.';
                 } else if(map[i][j] == 'B') {
-                    B = new Point(i, j);
+                    by = i;
+                    bx = j;
                     map[i][j] = '.';
-                }
-                if(map[i][j] == '.') {
-                    visit.put(i * M + j, new HashSet<>());
-                    visitB.put(i * M + j, new HashSet<>());
                 }
             }
         }
 
-        bw.write(solve(new Movement(A, B, 0)) + "");
+        bw.write(solve(new Movement(ay, ax, by, bx, 0)) + "");
 
         bw.flush();
         bw.close();
@@ -112,22 +103,18 @@ public class bfs_01580_changePosition {
     }
 
     static class Movement {
-        Point a;
-        Point b;
+        int ay;
+        int ax;
+        int by;
+        int bx;
         int n;
-        public Movement(Point a, Point b, int n) {
-            this.a = a;
-            this.b = b;
-            this.n = n;
-        }
-    }
 
-    static class Point {
-        int y;
-        int x;
-        public Point(int y, int x) {
-            this.y = y;
-            this.x = x;
+        public Movement(int ay, int ax, int by, int bx, int n) {
+            this.ay = ay;
+            this.ax = ax;
+            this.by = by;
+            this.bx = bx;
+            this.n = n;
         }
     }
 
