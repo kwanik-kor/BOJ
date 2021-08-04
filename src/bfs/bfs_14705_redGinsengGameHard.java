b7e3b24fa404652bd603dd60db03158cf147a8f3
kwanik-kor <rhksdlr134@naver.com>
2021-08-04 21:17:21 +0900

    Problem Solving: 14705번 홍삼 게임(Hard)

--- src/bfs/bfs_14705_redGinsengGameHard.java
+++ src/bfs/bfs_14705_redGinsengGameHard.java
@@ -1,2 +1,30 @@
-package bfs;public class bfs_14705_redGinsengGameHard {
+package bfs;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 진행 중인 게임을 회전시켜도 답이 바뀌지는 않습니다.
+ * 두 지목권의 위치를 상대 위치로 관리하면 상태가 2𝑁 개로
+ * 줄어듭니다.
+ */
+public class bfs_14705_redGinsengGameHard {
+
+    static int N, A, B, dA, dB;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        dA = Integer.parseInt(st.nextToken());
+        dB = Integer.parseInt(st.nextToken());
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
