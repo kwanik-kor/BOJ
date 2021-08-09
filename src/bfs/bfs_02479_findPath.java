856b57774f831f6d867c3ebbda072e5b482b6494
kwanik-kor <rhksdlr134@naver.com>
2021-08-09 10:44:18 +0900

    Problem Solved: 2479번 경로 찾기

--- src/bfs/bfs_02479_findPath.java
+++ src/bfs/bfs_02479_findPath.java
@@ -1,12 +1,62 @@
 package bfs;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
+/**
+ * 1. 문제 링크 : https://www.acmicpc.net/problem/2479
+ * 2. 풀이
+ *  - 주어진 코드를 Integer로 변경해서 두 수의 차이가 2의 제곱수인가를 체크 했었으나
+ *    반례가 존재하는 것으로 보임
+ *  - 순수하게 문자열을 파싱해서 해밍 거리를 잡아주니 통과 함
+ */
 public class bfs_02479_findPath {
 
-    static int N, K;
-    static String[] nodes;
+    static int N, K, startIdx, endIdx;
+    static char[][] codes;
+    static int[] path;
+    static Map<Integer, Set<Integer>> map = new HashMap<>();
+
+    static boolean bfs() {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(startIdx);
+        path[startIdx] = startIdx;
+
+        while(!q.isEmpty()) {
+            int now = q.poll();
+            if(now == endIdx) {
+                return true;
+            }
+
+            for(Integer next : map.get(now)) {
+                if(path[next] != -1) continue;
+                path[next] = now;
+                q.add(next);
+            }
+        }
+
+        return false;
+    }
+
+    static void setMap() {
+        for(int i = 1; i < N; i++) {
+            for(int j = i + 1; j <= N; j++) {
+                if(isHammingPath(codes[i], codes[j])) {
+                    map.get(i).add(j);
+                    map.get(j).add(i);
+                }
+            }
+        }
+    }
+
+    static boolean isHammingPath(char[] a, char[] b) {
+        int cnt = 0;
+        for(int i = 0; i < K; i++) {
+            if(a[i] != b[i]) cnt++;
+            if(cnt > 1) return false;
+        }
+        return cnt == 1;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,9 +65,36 @@ public class bfs_02479_findPath {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        nodes = new String[N + 1];
+        codes = new char[N + 1][K];
+        path = new int[N + 1];
         for(int i = 1; i <= N; i++) {
-            nodes[i] = br.readLine();
+            codes[i] = br.readLine().toCharArray();
+            path[i] = -1;
+            map.put(i, new HashSet<>());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        startIdx = Integer.parseInt(st.nextToken());
+        endIdx = Integer.parseInt(st.nextToken());
+
+        setMap();
+
+        if(!bfs()) {
+            bw.write("-1");
+        } else {
+            StringBuilder sb = new StringBuilder("");
+            int end = endIdx;
+            int start = path[endIdx];
+            while(true) {
+                sb.insert(0, end + " ");
+                end = start;
+                start = path[end];
+                if(start == end) {
+                    sb.insert(0, start + " ");
+                    break;
+                }
+            }
+            bw.write(sb.toString());
         }
 
         bw.flush();
