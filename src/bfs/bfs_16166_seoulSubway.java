914dd9b401df22ad275c04566bfb455e9425306e
kwanik-kor <rhksdlr134@naver.com>
2021-08-28 21:47:29 +0900

    Problem Solving...

--- src/bfs/bfs_16166_seoulSubway.java
+++ src/bfs/bfs_16166_seoulSubway.java
@@ -1,4 +1,15 @@
 package bfs;
 
+import java.io.*;
+
 public class bfs_16166_seoulSubway {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
