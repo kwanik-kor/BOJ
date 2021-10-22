403a7ee2e6455cf30f21d87968c4fd5ea279c698
kwanik-kor <rhksdlr134@naver.com>
2021-10-22 13:28:17 +0900

    Problem Solving...

---
+++ src/ds/ds_trie_03080_beautifulName.java
@@ -0,0 +1,30 @@
+package ds;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class ds_trie_03080_beautifulName {
+
+    static final int MOD = 1000000007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static class Trie {
+
+    }
+
+    static class TrieNode {
+
+    }
+}
