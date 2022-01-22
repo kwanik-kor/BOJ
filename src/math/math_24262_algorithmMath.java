7ceeea7929b8a8315c32d2a28b105c2551dc5d31
kwanik-kor <rhksdlr134@naver.com>
2022-01-22 23:12:26 +0900

    Problem Solved: 24262번 알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ src/math/math_24262_algorithmMath.java
@@ -0,0 +1,9 @@
+package math;
+
+import java.io.IOException;
+
+public class math_24262_algorithmMath {
+    public static void main(String[] args) throws IOException {
+        System.out.println("1\n0");
+    }
+}
