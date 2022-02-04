894482a12ea09119941cc3e242ea2aa7cf983893
kwanik-kor <rhksdlr134@naver.com>
2022-02-04 20:23:00 +0900

    Problem Solved: 23825번 SASA 모형을 만들어보자

---
+++ src/math/math_23825_sasaModel.java
@@ -0,0 +1,24 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/23825
+ */
+public class math_23825_sasaModel {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        bw.write(Math.min(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())) / 2 + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
