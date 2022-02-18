51b6db211371b1f75ca02544a9e3213598d748ef
kwanik-kor <rhksdlr134@naver.com>
2022-02-18 09:06:38 +0900

    Problem Solved: 24072번 帰省 (Homecoming)

---
+++ src/math/math_24072_homecoming.java
@@ -0,0 +1,28 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크 : https://www.acmicpc.net/problem/24072
+ */
+public class math_24072_homecoming {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        bw.write(a <= c && c < b ? "1" : "0");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
