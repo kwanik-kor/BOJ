f64ebe5b53f15acfa2f3b7b0db76d260ec8e286d
kwanik-kor <rhksdlr134@naver.com>
2022-02-21 14:07:44 +0900

    Problem Solved: 22015번 金平糖 (Konpeito)

---
+++ src/math/math_22015_konpeito.java
@@ -0,0 +1,30 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크 : https://www.acmicpc.net/problem/22015
+ */
+public class math_22015_konpeito {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int max = Math.max(A, Math.max(B, C));
+
+        bw.write(max * 3 - (A + B + C) + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
