c28ca7fff139245f5fb5259a55c215ffb2206162
kwanik-kor <rhksdlr134@naver.com>
2022-02-25 22:45:31 +0900

    Problem Solved: 24365번 ПЧЕЛИЧКАТА МАЯ

---
+++ src/math/math_24365_beePosition.java
@@ -0,0 +1,29 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크 : https://www.acmicpc.net/problem/24365
+ */
+public class math_24365_beePosition {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+
+        bw.write(Math.min(B + C - 2 * A, Math.min(C - A, C * 2 - B - A)) + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
