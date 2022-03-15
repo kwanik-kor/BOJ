b4028fd4369bfd985341891c85eb3ccc595f0b51
kwanik-kor <rhksdlr134@naver.com>
2022-03-15 23:44:19 +0900

    Problem Solved: 8871번 Zadanie próbne 2

---
+++ src/math/math_08871_zadaniProbne.java
@@ -0,0 +1,23 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/8871
+ */
+public class math_08871_zadaniProbne {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        bw.write(2 * (N + 1) + " " + (3 * (N + 1)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
