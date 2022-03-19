26da79fd7e3419cec72178243c1592c7822a3a04
kwanik-kor <rhksdlr134@naver.com>
2022-03-19 12:18:56 +0900

    Problem Solved: 9063번 대지

---
+++ src/math/math_09063_marvel.java
@@ -0,0 +1,34 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/9063
+ */
+public class math_09063_marvel {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int minX = 10001, minY = 10001, maxX = -10001, maxY = -10001;
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+        bw.write(Math.abs(maxX - minX) * Math.abs(maxY - minY) + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
