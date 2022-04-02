7086d2b6bf9ff8ac248ab58bab0ca004244e471d
kwanik-kor <rhksdlr134@naver.com>
2022-04-02 14:44:35 +0900

    Problem Solved: 24218번 Double Crypt 1

---
+++ src/simulation/sml_24218_doubleCrypt.java
@@ -0,0 +1,12 @@
+package simulation;
+
+import java.io.IOException;
+
+/**
+ * 1. 문제 링크 : https://www.acmicpc.net/problem/24218
+ */
+public class sml_24218_doubleCrypt {
+    public static void main(String[] args) throws IOException {
+        System.out.println("A0000000000000000000000000000000\n70000000000000000000000000000000");
+    }
+}
