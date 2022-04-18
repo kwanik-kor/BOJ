60d62938b20a29b87b38ea2e8e3afb8e863f83f4
kwanik-kor <rhksdlr134@naver.com>
2022-04-18 22:07:51 +0900

    Problem Solved: 8545번 Zadanie próbne

---
+++ src/simulation/sml_08545_zadanieProbne.java
@@ -0,0 +1,22 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/8545
+ */
+public class sml_08545_zadanieProbne {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(new StringBuilder(br.readLine()).reverse().toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
