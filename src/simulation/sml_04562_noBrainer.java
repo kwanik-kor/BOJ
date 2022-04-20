55b7a02494f56fb710042538f45a4db9afab0149
kwanik-kor <rhksdlr134@naver.com>
2022-04-21 08:35:50 +0900

    Problem Solved: 4562번 No Brainer

---
+++ src/simulation/sml_04562_noBrainer.java
@@ -0,0 +1,29 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/4562
+ */
+public class sml_04562_noBrainer {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        while(N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a >= b? "MMM BRAINS\n" : "NO BRAINS\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
