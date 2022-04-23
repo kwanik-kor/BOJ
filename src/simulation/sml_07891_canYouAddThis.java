9e778415ecf4c62c7570c6ade90ba6a5e9c31438
kwanik-kor <rhksdlr134@naver.com>
2022-04-23 23:29:30 +0900

    Problem Solved: 7891번 Can you add this?

---
+++ src/simulation/sml_07891_canYouAddThis.java
@@ -0,0 +1,28 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/7891
+ */
+public class sml_07891_canYouAddThis {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
