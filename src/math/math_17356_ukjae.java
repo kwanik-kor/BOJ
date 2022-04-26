819a7d83873f971fcc3ee9f14eb703fb96d44f6a
kwanik-kor <rhksdlr134@naver.com>
2022-04-26 10:57:51 +0900

    Problem Solved: 17356번 욱 제

---
+++ src/math/math_17356_ukjae.java
@@ -0,0 +1,25 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/17356
+ */
+public class math_17356_ukjae {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+        bw.write(String.format("%.10f", 1d / (1d + Math.pow(10d, (b - a) / 400d))));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
