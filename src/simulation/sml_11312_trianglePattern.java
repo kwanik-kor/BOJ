40654ca914797d83ceaea74d18e590366e2a3817
kwanik-kor <rhksdlr134@naver.com>
2022-06-11 15:01:48 +0900

    Problem Solved: 11312번 삼각 무늬 - 2

---
+++ src/simulation/sml_11312_trianglePattern.java
@@ -0,0 +1,35 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/11312
+ */
+public class sml_11312_trianglePattern {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int cnt = A / B;
+            long sum = 0L, flag = -1L;
+            for (int i = 1; i <= cnt; i++) {
+                flag += 2;
+                sum += flag;
+            }
+            bw.write(sum + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
