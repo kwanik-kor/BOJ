04b28d01d08f6e6c978c0c0d6d2c8d3661b90f07
kwanik-kor <rhksdlr134@naver.com>
2022-06-14 17:50:11 +0900

    Problem Solved: 2566번 최댓값

---
+++ src/simulation/sml_02566_max.java
@@ -0,0 +1,37 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/2566
+ */
+public class sml_02566_max {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int max = 0;
+        int row = 0, col = 1;
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                int n = Integer.parseInt(st.nextToken());
+                if(n > max) {
+                    max = n;
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        bw.write(String.format("%d\n%d %d", max, row + 1, col + 1));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
