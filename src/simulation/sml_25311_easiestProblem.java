27e9f5ba06559c02fce3eaa5fb38a6cb329cf0ae
kwanik-kor <rhksdlr134@naver.com>
2022-07-04 20:34:32 +0900

    Problem Solved: 25311번 UCPC에서 가장 쉬운 문제 번호는?

---
+++ src/simulation/sml_25311_easiestProblem.java
@@ -0,0 +1,10 @@
+package simulation;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/25311
+ */
+public class sml_25311_easiestProblem {
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+}
