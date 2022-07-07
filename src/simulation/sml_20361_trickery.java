6bd07c99477d191f82c65f087879df135bad11ec
kwanik-kor <rhksdlr134@naver.com>
2022-07-08 08:37:17 +0900

    Problem Solved: 20361번 일우는 야바위꾼

---
+++ src/simulation/sml_20361_trickery.java
@@ -0,0 +1,37 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/20361
+ */
+public class sml_20361_trickery {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int idx = X;
+        while (K-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if (A == idx) idx = B;
+            else if (B == idx) idx = A;
+        }
+
+        bw.write(idx + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
