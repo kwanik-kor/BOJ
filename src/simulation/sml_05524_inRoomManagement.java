8e70e6157625de99750c1d8e2aaca1221ab1f128
kwanik-kor <rhksdlr134@naver.com>
2022-07-16 16:36:54 +0900

    Problem Solved: 5524번 입실 관리

---
+++ src/simulation/sml_05524_inRoomManagement.java
@@ -0,0 +1,25 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/5524
+ */
+public class sml_05524_inRoomManagement {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            bw.write(br.readLine().toLowerCase() + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
