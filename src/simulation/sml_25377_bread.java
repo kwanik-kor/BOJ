226afede91a8b31f3c0000b4975c5a7e0b2e34df
kwanik-kor <rhksdlr134@naver.com>
2022-07-21 20:44:17 +0900

    Problem Solved: 25377번 빵

---
+++ src/simulation/sml_25377_bread.java
@@ -0,0 +1,32 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/25377
+ */
+public class sml_25377_bread {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int ans = 1001;
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a > b) continue;
+            ans = Math.min(ans, b);
+        }
+        bw.write(ans == 1001 ? "-1" : ans + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
