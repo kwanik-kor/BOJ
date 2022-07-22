84e968760977ce35effb0f7dbbcbbfb1d9f323ee
kwanik-kor <rhksdlr134@naver.com>
2022-07-22 20:47:52 +0900

    Problem Solved: 15000번 CAPS

---
+++ src/simulation/sml_15000_caps.java
@@ -0,0 +1,22 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/15000
+ */
+public class sml_15000_caps {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(br.readLine().toUpperCase());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
