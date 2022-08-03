760147df47937a0ea01373797ed41029d5d11f5f
kwanik-kor <rhksdlr134@naver.com>
2022-08-03 20:47:08 +0900

    Problem Solved: 2669번 직사각형 네개의 합집합의 면적 구하기

---
+++ src/simulation/sml_02669_area.java
@@ -0,0 +1,41 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/2669
+ */
+public class sml_02669_area {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 4; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for (int x = x1; x < x2; x++) {
+                for (int y = y1; y < y2; y++) {
+                    set.add(x * 100 + y);
+                }
+            }
+        }
+        bw.write(set.size() + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
