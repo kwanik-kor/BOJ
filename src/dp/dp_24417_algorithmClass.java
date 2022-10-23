feda1182918ac780f28c1e5912f8d26c6677a189
kwanik-kor <rhksdlr134@naver.com>
2022-10-24 08:09:30 +0900

    Problem Solved: 24417번 알고리즘 수업 - 피보나치 수 2

---
+++ src/dp/dp_24417_algorithmClass.java
@@ -0,0 +1,31 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/24417
+ */
+public class dp_24417_algorithmClass {
+
+    static final int MOD = 1000000007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+        bw.write(dp[n] + " " + (n - 2));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
