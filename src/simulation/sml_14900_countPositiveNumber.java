01072958dccc4d0112360307175e962ff3f61175
kwanik-kor <rhksdlr134@naver.com>
2022-10-28 09:23:45 +0900

    Problem Solved: 14900번 양수 개수 세기

---
+++ src/simulation/sml_14900_countPositiveNumber.java
@@ -0,0 +1,27 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/14909
+ */
+public class sml_14900_countPositiveNumber {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .filter(num -> num > 0)
+                        .toArray();
+        bw.write(arr.length + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
