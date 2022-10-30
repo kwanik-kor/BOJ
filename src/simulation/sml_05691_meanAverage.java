cea9766986849b84927b0d8dd23c9c82a5ffecbd
kwanik-kor <rhksdlr134@naver.com>
2022-10-30 13:50:41 +0900

    Problem Solved: 5691번 평균 중앙값 문제

---
+++ src/simulation/sml_05691_meanAverage.java
@@ -0,0 +1,32 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/5691
+ */
+public class sml_05691_meanAverage {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if (A == 0 && B == 0) {
+                break;
+            }
+            bw.write(2 * A - B + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
