497f7e124c5e38c1dc36cdce47ff18be7bc37239
kwanik-kor <rhksdlr134@naver.com>
2022-11-23 19:36:27 +0900

    Problem Solved: 24264번 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ src/simulation/sml_24264_algorithmClass3.java
@@ -0,0 +1,23 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/24264
+ */
+public class sml_24264_algorithmClass3 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long N = Long.parseLong(br.readLine());
+        bw.write(N * N + "\n2");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
