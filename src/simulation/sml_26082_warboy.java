8ac1f5db7e19b96f9d9075fd86ec426ffc15d20a
kwanik-kor <rhksdlr134@naver.com>
2022-11-28 20:12:14 +0900

    Problem Solved: 26082번 WARBOY

---
+++ src/simulation/sml_26082_warboy.java
@@ -0,0 +1,27 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/26082
+ */
+public class sml_26082_warboy {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        bw.write(B / A * C * 3 + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
