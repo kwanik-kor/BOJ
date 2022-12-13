ed0a256c6dd129b4055a58b016c7e4017677799d
kwanik-kor <rhksdlr134@naver.com>
2022-12-13 20:38:33 +0900

    Problem Solved: 26037번 Correct

---
+++ src/simulation/sml_26037_correct.java
@@ -0,0 +1,26 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/26307
+ */
+public class sml_26037_correct {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        bw.write((H - 9) * 60 + M + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
