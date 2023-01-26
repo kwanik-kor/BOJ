2fa619ebd15e8aa35cd1680e7468d0eac1450b97
kwanik-kor <rhksdlr134@naver.com>
2023-01-26 20:50:45 +0900

    Problem Solved: 6840번 Who is in the middle?

---
+++ src/simulation/sml_06840_whoIsInTheMiddle.java
@@ -0,0 +1,28 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+/**
+ * 1. 문제 링크:  https://www.acmicpc.net/problem/6840
+ */
+public class sml_06840_whoIsInTheMiddle {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        bw.write(arr[1] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
