ff94e864f84d3fe3069c63b1d84fed74f3b25d13
kwanik-kor <rhksdlr134@naver.com>
2023-02-16 16:47:55 +0900

    Problem Solved: 9742번 순열

--- src/backtracking/backtracking_09742_permutation.java
+++ src/backtracking/backtracking_09742_permutation.java
@@ -9,36 +9,12 @@ import java.util.StringTokenizer;
 
 public class backtracking_09742_permutation {
 
-    static int[] dp = new int[11];
+    static int[] fact = new int[11];
 
-    static char[] arr;
+    static char[] arr, chars;
+    static boolean[] visit;
     static int n, cnt;
     static String ans;
-
-    static void calcFactorial() {
-        dp[0] = 1;
-        for (int i = 1; i <= 10; i++) {
-            dp[i] = dp[i - 1] * i;
-        }
-    }
-
-    static void solveWithBacktracking(int len, String str, boolean[] visit) {
-        if (len == arr.length) {
-            cnt++;
-            if (cnt == n) {
-                ans = str;
-                return;
-            }
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            if (visit[i]) continue;
-
-            visit[i] = true;
-            solveWithBacktracking(len + 1, str + arr[i], visit);
-            visit[i] = false;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -53,18 +29,54 @@ public class backtracking_09742_permutation {
             StringTokenizer st = new StringTokenizer(line);
             arr = st.nextToken().toCharArray();
             n = Integer.parseInt(st.nextToken());
-            ans = "";
-            cnt = 0;
 
-            if (n > dp[arr.length]) {
+            cnt = 0;
+            visit = new boolean[arr.length];
+            chars = new char[arr.length];
+
+            if (n > fact[arr.length]) {
                 bw.write("No permutation\n");
             } else {
-                solveWithBacktracking(0, "", new boolean[arr.length]);
-                bw.write(ans + "\n");
+                bw.write(solveWithBacktracking() + "\n");
             }
         }
 
         bw.close();
         br.close();
+    }
+
+    static void calcFactorial() {
+        fact[0] = 1;
+        for (int i = 1; i <= 10; i++) {
+            fact[i] = fact[i - 1] * i;
+        }
+    }
+
+    static String solveWithBacktracking() {
+        cnt = 0;
+        visit = new boolean[arr.length];
+        chars = new char[arr.length];
+
+        backtracking(0);
+
+        return ans;
+    }
+
+    static void backtracking(int len) {
+        if (len == arr.length) {
+            cnt++;
+            if (cnt == n) {
+                ans = new String(chars);
+                return;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (visit[i]) continue;
+            visit[i] = true;
+            chars[len] = arr[i];
+            backtracking(len + 1);
+            visit[i] = false;
+        }
     }
 }
