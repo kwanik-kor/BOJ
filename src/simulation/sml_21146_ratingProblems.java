b26f708dbb61418bc007a31e6cfe16f7f90227ab
kwanik-kor <rhksdlr134@naver.com>
2023-02-25 08:11:50 +0900

    Problem Solved: 27433 팩토리얼 2

--- src/simulation/sml_21146_ratingProblems.java
+++ src/simulation/sml_21146_ratingProblems.java
@@ -5,6 +5,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
