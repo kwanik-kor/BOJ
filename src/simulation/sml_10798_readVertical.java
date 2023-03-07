4c8c00565cf9efbdbc9e92ebac821139528a6915
kwanik-kor <rhksdlr134@naver.com>
2023-03-07 17:40:58 +0900

    Problem Solved: 10798번 세로읽기

---
+++ src/simulation/sml_10798_readVertical.java
@@ -0,0 +1,36 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/10798
+ */
+public class sml_10798_readVertical {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char[][] arr = new char[5][15];
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                arr[i][j] = line.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] != '\u0000') {
+                    bw.write(arr[j][i]);
+                }
+            }
+        }
+
+        bw.close();
+        br.close();
+    }
+}
