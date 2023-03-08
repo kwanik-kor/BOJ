0417da345503b27547f6bd4c5b7089ac505407a4
kwanik-kor <rhksdlr134@naver.com>
2023-03-08 09:13:38 +0900

    Problem Solved: 2559번 수열

---
+++ src/prefixSum/ps_02559_sequence.java
@@ -0,0 +1,38 @@
+package prefixSum;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/2559
+ */
+public class ps_02559_sequence {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = K; i <= N; i++) {
+            max = Math.max(max, arr[i] - arr[i - K]);
+        }
+
+        bw.write(max + "");
+        bw.close();
+        br.close();
+    }
+
+}
