b2317baf7aff7be6cbfc69892181edb6a3e95a0c
kwanik-kor <rhksdlr134@naver.com>
2023-03-13 08:22:08 +0900

    Problem Solved: 4796번 캠핑

---
+++ src/math/math_04796_camping.java
@@ -0,0 +1,36 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/4796
+ */
+public class math_04796_camping {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        for (int i = 1; true; i++) {
+            st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            if (L == 0) {
+                break;
+            }
+
+            bw.write(String.format("Case %d: %d\n", i, V / P * L + Math.min(V % P, L)));
+        }
+
+        bw.close();
+        br.close();
+    }
+}
