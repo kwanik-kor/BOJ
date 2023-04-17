f20ad3a2b875d79c4e126bd86d9d8af8bc868de4
kwanik-kor <rhksdlr134@naver.com>
2023-04-17 18:26:41 +0900

    Problem Solving: 2873번 롤러코스터

--- src/greedy/greedy_02873_rollerCoaster.java
+++ src/greedy/greedy_02873_rollerCoaster.java
@@ -7,9 +7,8 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class greedy_02873_rollerCoaster {
-    static final String DIRECTION = "URDL";
-    static final int[] dy = {-1, 0, 1, 0};
-    static final int[] dx = {0, 1, 0, -1};
+    static final String DIRECTION = "UD";
+    static final int[] dy = {-1, 1};
 
     static int R, C;
 
@@ -23,19 +22,11 @@ public class greedy_02873_rollerCoaster {
         int minPointIdx = initializeAndGetMinPlace();
         if (R % 2 == 1 || C % 2 == 1) {
             bw.write(createAllNodePaths());
+        } else {
+            bw.write(createEvenPaths(minPointIdx));
         }
 
         bw.close();
-    }
-
-    /**
-     * 가로 혹은 세로 길이가 홀수인 경우 전체 경로를 탐색할 수 있음
-     * @return 모든 노드를 탐색하는 경로
-     */
-    private String createAllNodePaths() {
-        StringBuilder sb = new StringBuilder();
-
-        return sb.toString();
     }
 
     private int initializeAndGetMinPlace() throws Exception {
@@ -50,7 +41,7 @@ public class greedy_02873_rollerCoaster {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < C; j++) {
                 int n = Integer.parseInt(st.nextToken());
-                if (isNotStartOrEndPoint(i, j) && n <= min) {
+                if (isNodeToCheckMinPoint(i, j) && n <= min) {
                     idx = i * C + j;
                     min = n;
                 }
@@ -61,8 +52,117 @@ public class greedy_02873_rollerCoaster {
         return idx;
     }
 
-    private boolean isNotStartOrEndPoint(int y, int x) {
-        int nodeIdx = y * C + x;
-        return nodeIdx != 0 && nodeIdx != R * C - 1;
+    /**
+     * 가로 혹은 세로 길이가 홀수인 경우 전체 경로를 탐색할 수 있음
+     *  - 행 개수가 짝수인 경우 (아래 > 오른쪽 > 위)
+     *  - 행 개수가 홀수인 경우 (오른쪽 > 아래 > 왼쪽)
+     * @return 모든 노드를 탐색하는 경로
+     */
+    private String createAllNodePaths() {
+        StringBuilder sb = new StringBuilder();
+        int first = getAllNodePathFirstDimensionMax();
+        int second = getAllNodePathSecondDimensionMax();
+        char[] directions = getAllNodePathDirections();
+
+        for (int i = 0; i < first; i++) {
+            for (int j = 0; j < second; j++) {
+                sb.append(i % 2 == 0 ? directions[0] : directions[1]);
+            }
+            if (i != first - 1) {
+                sb.append(directions[2]);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String createEvenPaths(int minIdx) {
+        StringBuilder sb = new StringBuilder();
+
+        int minRow = minIdx / C;
+
+        boolean startFromLeft = true;
+        for (int row = 0, max = R / 2; row < max; row++) {
+            if (row * 2 == minRow || (row * 2 + 1) == minRow) {
+                sb.append(colFirstSearch(row * 2, minIdx));
+                startFromLeft = false;
+            } else {
+                sb.append(rowFirstSearch(row * 2, startFromLeft));
+            }
+        }
+        return sb.toString();
+    }
+
+    private String rowFirstSearch(int startRow, boolean startFromLeft) {
+        StringBuilder sb = new StringBuilder();
+
+        char[] directions = getRowSearchDirections(startFromLeft);
+        for (int y = 0; y < 2; y++) {
+            for (int x = 0; x < C - 1; x++) {
+                sb.append(directions[y]);
+            }
+            if (y == 0) {
+                sb.append('D');
+            }
+        }
+        if (startRow + 1 != R - 1) {
+            sb.append('D');
+        }
+
+        return sb.toString();
+    }
+
+    private String colFirstSearch(int startRow, int passNodeIdx) {
+        StringBuilder sb = new StringBuilder();
+
+        int colDirection = 1;
+        int y = startRow;
+        for (int x = 0; x < C; x++) {
+            int ny = y + dy[colDirection];
+
+            if (ny == passNodeIdx / C && x == passNodeIdx % C) {
+                if (!(ny == R - 1 && x == C - 1)) {
+                    sb.append('R');
+                }
+                continue;
+            }
+
+            sb.append(DIRECTION.charAt(colDirection));
+            if (!((ny == R - 1 || ny + 1 == R - 1) && (x + 1 == C - 1 || x == C - 1))) {
+                sb.append('R');
+            }
+            y = ny;
+            colDirection ^= 1;
+        }
+
+        if (startRow + 1 != R - 1) {
+            sb.append('D');
+        }
+
+        return sb.toString();
+    }
+
+    private boolean isNodeToCheckMinPoint(int y, int x) {
+        return (y + x) % 2 == 1;
+    }
+
+    private int getAllNodePathFirstDimensionMax() {
+        return R % 2 == 0 ? C : R;
+    }
+
+    private int getAllNodePathSecondDimensionMax() {
+        return (R % 2 == 0 ? R : C) - 1;
+    }
+
+    private char[] getAllNodePathDirections() {
+        return R % 2 == 0
+                ? new char[]{'D', 'U', 'R'}
+                : new char[]{'R', 'L', 'D'};
+    }
+
+    private char[] getRowSearchDirections(boolean startFromLeft) {
+        return startFromLeft
+                ? new char[]{'R', 'L'}
+                : new char[]{'L', 'R'};
     }
 }
