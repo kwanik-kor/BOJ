61a36770332ae758493a1ed4e0421fcfc4d933f5
kwanik-kor <rhksdlr134@naver.com>
2023-04-18 19:44:42 +0900

    Problem Solved: 13448번 SW 역량 테스트

--- src/greedy/greedy_13448_SwCompetencyTest.java
+++ src/greedy/greedy_13448_SwCompetencyTest.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -14,25 +15,76 @@ public class greedy_13448_SwCompetencyTest {
     static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     static int N, T;
-
+    static long[][] dp;
 
     public static void main(String[] args) throws Exception {
         new greedy_13448_SwCompetencyTest().solve();
 
         bw.close();
-        br.close();
     }
 
     private void solve() throws Exception {
+        Problem[] problems = initializeAndReturnSortedProblems();
 
+        for (int i = 1; i <= N; i++) {
+            for (int t = 1; t <= T; t++) {
+                long score = 0;
+                if (t - problems[i].r >= 0) {
+                    score = dp[i - 1][t - problems[i].r] + problems[i].getScore(t);
+                }
+                dp[i][t] = Math.max(dp[i - 1][t], score);
+            }
+        }
+
+        long maxScore = 0;
+        for (int i = 1; i <= T; i++) {
+            maxScore = Math.max(maxScore, dp[N][i]);
+        }
+
+        bw.write(maxScore + "");
     }
 
-    private void initialize() throws Exception {
+    private Problem[] initializeAndReturnSortedProblems() throws Exception {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
 
+        dp = new long[N + 1][T + 1];
 
+        StringTokenizer constant = new StringTokenizer(br.readLine());
+        StringTokenizer time = new StringTokenizer(br.readLine());
+        StringTokenizer duration = new StringTokenizer(br.readLine());
+        br.close();
+
+        Problem[] problems = new Problem[N + 1];
+        for (int i = 1; i <= N; i++) {
+            problems[i] = new Problem(
+                    Integer.parseInt(constant.nextToken()),
+                    Integer.parseInt(time.nextToken()),
+                    Integer.parseInt(duration.nextToken())
+            );
+        }
+        Arrays.sort(problems, 1, N + 1);
+        return problems;
+    }
+
+    private static class Problem implements Comparable<Problem> {
+        int m, p, r;
+
+        public Problem(int m, int p, int r) {
+            this.m = m;
+            this.p = p;
+            this.r = r;
+        }
+
+        public long getScore(int t) {
+            return (long) m - (long) t * p;
+        }
+
+        @Override
+        public int compareTo(Problem o) {
+            return Double.compare(((double) o.p) / o.r, ((double) p) / r);
+        }
     }
 
 }
