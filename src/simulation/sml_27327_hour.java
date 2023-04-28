6b6a3fbd93b1ebf7917d39b885403831842327a2
kwanik-kor <rhksdlr134@naver.com>
2023-04-28 23:33:10 +0900

    Problem Solved: 27327번 時間 (Hour)

---
+++ src/simulation/sml_27327_hour.java
@@ -0,0 +1,21 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/27327
+ */
+public class sml_27327_hour {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(Integer.parseInt(br.readLine()) * 24 + "");
+        bw.close();
+        br.close();
+    }
+}
