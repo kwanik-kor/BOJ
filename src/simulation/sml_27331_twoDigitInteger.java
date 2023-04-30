ac35b11122a2367bde943248a504aa4f8cfbeaf4
kwanik-kor <rhksdlr134@naver.com>
2023-04-30 23:25:28 +0900

    Problem Solved: 27331번 2 桁の整数 (Two-digit Integer)

---
+++ src/simulation/sml_27331_twoDigitInteger.java
@@ -0,0 +1,20 @@
+package simulation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/27331
+ */
+public class sml_27331_twoDigitInteger {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(br.readLine() + br.readLine());
+        bw.close();
+        br.close();
+    }
+}
