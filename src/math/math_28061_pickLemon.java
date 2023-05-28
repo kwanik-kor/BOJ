7a0383e42b7b723d7480e29a51f6b3105a24704f
kwanik-kor <rhksdlr134@naver.com>
2023-05-28 21:51:08 +0900

    Problem Solved: 28061번 레몬 따기

---
+++ src/math/math_28061_pickLemon.java
@@ -0,0 +1,28 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * 1. 문제 링크: https://www.acmicpc.net/problem/28061
+ */
+public class math_28061_pickLemon {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int max = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            max = Math.max(max, n - (N - i));
+        }
+        bw.write(max + "");
+        bw.close();
+        br.close();
+    }
+}
