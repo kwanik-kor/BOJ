c9ad44b5df28d26eecc8100abdeeddf2370b7ab9
kwanik-kor <rhksdlr134@naver.com>
2025-06-24 15:15:33 +0900

    Problem Solved: 28235번 코드마스터 2023
    https://www.acmicpc.net/problem/28235

---
+++ src/BOJ_28200/BOJ_28235.kt
@@ -0,0 +1,12 @@
+package BOJ_28200
+
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    when (nextLine()) {
+        "SONGDO" -> println("HIGHSCHOOL")
+        "CODE" -> println("MASTER")
+        "2023" -> println("0611")
+        else -> println("CONTEST")
+    }
+}
