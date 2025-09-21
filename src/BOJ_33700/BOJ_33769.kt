a769fb40ad9a42be08b2f3ca53475ae049de83e7
kwanik-kor <rhksdlr134@naver.com>
2025-09-21 17:15:57 +0900

    feat: 6219번 소수의 자격

---
+++ src/BOJ_33700/BOJ_33769.kt
@@ -0,0 +1,21 @@
+package BOJ_33700
+
+import kotlin.math.sqrt
+
+fun main() {
+    val (a, b, d) = readLine()!!.split(" ").map { it.toInt() }
+    val eratos = BooleanArray(b + 1) { true }
+    for (i in 2 .. sqrt(b.toDouble()).toInt()) {
+        if (!eratos[i]) continue
+
+        for (j in i + i .. b step i) {
+            eratos[j] = false
+        }
+    }
+
+    val ch = d.toString()
+    val ans = (a .. b).filter { eratos[it] }.count {
+        it.toString().contains(ch)
+    }
+    println(ans)
+}
