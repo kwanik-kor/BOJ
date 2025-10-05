9f1d2fe5662e824d584db7b3887c22a91f33b805
kwanik-kor <rhksdlr134@naver.com>
2025-10-05 10:18:21 +0900

    feat: 29766번 DKSH 찾기

--- src/BOJ_29700/BOJ_29766.kt
+++ src/BOJ_29700/BOJ_29766.kt
@@ -2,6 +2,13 @@ package BOJ_29700
 
 fun main() {
     val line = readLine()!!
-    val count = (0 until line.length - 4).count { line.substring(it, it + 4) == "DKSH" }
+    val name = "DKSH"
+    val count = (0 .. line.length - 4).count {
+        var flag = true
+        for (i in 0 until 4) {
+            if (line[it + i] != name[i]) flag = false
+        }
+        flag
+    }
     println(count)
 }
