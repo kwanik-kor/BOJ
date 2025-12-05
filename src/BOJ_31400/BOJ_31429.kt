058d75f87fbdfca9b0b8289313d02eb8a5f19fc6
kwanik-kor <rhksdlr134@naver.com>
2025-12-05 12:37:14 +0900

    feat: 31429번

---
+++ src/BOJ_31400/BOJ_31429.kt
@@ -0,0 +1,15 @@
+package BOJ_31400
+
+fun main() = when(readln().toInt()) {
+    1 ->  println("12 1600")
+    2 -> println("11 894")
+    3 -> println("11 1327")
+    4 -> println("10 1311")
+    5 -> println("9 1004")
+    6 -> println("9 1178")
+    7 -> println("9 1357")
+    8 -> println("8 837")
+    9 -> println("7 1055")
+    10 -> println("6 556")
+    else -> println("6 773")
+}
